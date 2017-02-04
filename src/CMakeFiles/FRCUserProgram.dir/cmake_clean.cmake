5b56f15409f4051d0226a1f4fd4a5ca3ab055962
Garrett Davidson <gdavids@purdue.edu>
2017-02-04 13:15:30 -0500

    Start cmake conversion

---
+++ src/CMakeFiles/FRCUserProgram.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/FRCUserProgram.dir/Robot.o"
+  "CMakeFiles/FRCUserProgram.dir/RateEncoder.o"
+  "FRCUserProgram.pdb"
+  "FRCUserProgram"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/FRCUserProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
