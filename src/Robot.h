54ad70199b9bde60d01155803a600c29cc7f7292
Garrett Davidson <gdavids@purdue.edu>
2017-03-08 01:44:28 -0500

    Migrate to Ports.h

--- src/Robot.h
+++ src/Robot.h
@@ -1,5 +1,7 @@
 #include "XboxJoystickMap.h"
 
+#include "Ports.h"
+
 enum RightGearHighGoal {
 	rGHG_LowGear,
 	rGHG_BackUp0,
@@ -117,77 +119,4 @@ enum AutoMode {
 	rGRHopperRed,
 	rGRHopperBlue,
 	highGoalReload
-};
-
-enum PWM {
-	frontLeftPWM = 0,
-	frontRightPWM = 2,
-	backLeftPWM = 1,
-	backRightPWM = 3,
-	intakeRollerPWM = 4,
-	climberPWM = 10,
-	leftShooterPWM = 9, //shooter 1
-	leftTowerPWM = 6,
-	rightShooterPWM = 8, //shooter 2
-	rightTowerPWM = 7,
-	conveyorPWM = 5
-};
-
-enum PCM {
-	intakeForwardPCM = 2,
-	intakeReversePCM = 3,
-	shifterForwardPCM = 0,
-	shifterReversePCM = 1
-};
-
-enum OperatorControls {
-	spinIntakeForwardButton = 11,
-	spinIntakeBackwardButton = 12,
-	climberButton = 2,
-	towersInButton = 7,
-	towersOutButton = 8,
-	shootingTowersConveyorButton = 1,
-	shootingButton = 3,
-	changeShooterSpeed = 0,
-	shootingModeSwitch = 6,
-	intakePositionSwitch = 5,
-	conveyorIn = 10,
-	conveyorOut = 9,
-	climberDownSwitch = 4 // TODO: Implement hood
-};
-/*
-enum HatControls {
-	lowerIntakePOV = 270,
-	raiseIntakePOV = 90,
-	conveyorInPOV = 0,
-	conveyorOutPOV = 180
-};
-*/
-
-enum DriverControlsXbox {
-	shiftGearsAxisXbox = 3
-};
-
-enum DriverControlsRightJoystick {
-	shiftGearsButtonRightJoystick = 1,
-};
-
-enum DriverControlsLeftJoystick {
-	driveStraightButtonLeftJoystick = 1,
-};
-
-enum DIO {	//digital input/output
-	leftDriveEncoderA = 1,
-	leftDriveEncoderB = 0,
-	rightDriveEncoderA = 2,
-	rightDriveEncoderB = 3,
-	leftShooterEncoderA = 4,
-	leftShooterEncoderB = 5,
-	rightShooterEncoderA = 6,
-	rightShooterEncoderB = 7
-};
-
-enum PDP_CHANNELS {
-	pdpConveyor = 7,
-	pdpIntake = 5
 };
