66387a17b575d388f087925c7d4d522b9852fa7f
Garrett Davidson <gdavids@purdue.edu>
2017-03-08 01:42:40 -0500

    Add operator controls for shooting

---
+++ src/OperatorControls.cpp
@@ -0,0 +1,54 @@
+/*
+ * OperatorControls.cpp
+ *
+ *  Created on: Mar 6, 2017
+ *      Author: WBI
+ */
+
+#include <OperatorControls.h>
+#include "Ports.h"
+
+OperatorControls::OperatorControls(int port) {
+  // TODO Auto-generated constructor stub
+  joystick = new Joystick(port);
+}
+
+OperatorControls::~OperatorControls() {
+  // TODO Auto-generated destructor stub
+}
+
+bool OperatorControls::IsAutomaticShootingMode() {
+  return !joystick->GetRawButton(shootingModeSwitch);
+}
+
+// Scales input from [1.0, -1.0] to [0.0, 1.0]
+double OperatorControls::ScaledShootingDial() {
+  return (-joystick->GetRawAxis(OperatorControlInputs::changeShooterSpeed)/2) + 0.5;
+}
+
+bool OperatorControls::IsAllOnShooting() {
+  return joystick->GetRawButton(shootingTowersConveyorButton);
+}
+
+bool OperatorControls::IsShooting() {
+  return joystick->GetRawButton(shootingButton);
+}
+
+Direction OperatorControls::GetConveyorDirection() {
+  if (joystick->GetRawButton(OperatorControlInputs::conveyorIn))
+    return Direction::kForward;
+  else if (joystick->GetRawButton(OperatorControlInputs::conveyorOut))
+    return Direction::kBackward;
+
+  return Direction::kOff;
+}
+
+Direction OperatorControls::GetTowerDirection() {
+  if (joystick->GetRawButton(OperatorControlInputs::towersInButton))
+    return Direction::kForward;
+
+  else if (joystick->GetRawButton(OperatorControlInputs::towersOutButton))
+    return Direction::kBackward;
+
+  return Direction::kOff;
+}
---
+++ src/OperatorControls.h
@@ -0,0 +1,38 @@
+/*
+ * OperatorControls.h
+ *
+ *  Created on: Mar 6, 2017
+ *      Author: WBI
+ */
+
+#ifndef SRC_OPERATORCONTROLS_H_
+#define SRC_OPERATORCONTROLS_H_
+
+#include <WPILib.h>
+
+enum Direction {
+  kForward,
+  kBackward,
+  kOff
+};
+
+class OperatorControls {
+public:
+  OperatorControls(int port);
+  virtual ~OperatorControls();
+
+  // Shooting
+  bool IsAutomaticShootingMode();
+
+  bool IsAllOnShooting();
+  bool IsShooting();
+  double ScaledShootingDial();
+
+  Direction GetConveyorDirection();
+  Direction GetTowerDirection();
+
+private:
+  Joystick *joystick;
+};
+
+#endif /* SRC_OPERATORCONTROLS_H_ */
